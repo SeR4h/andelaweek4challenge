7bdf1c0615000c41b56f0b16772a0d3dab53eef1
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-21 16:50:16 +0300

    News api with corrections

--- index.js
+++ index.js
@@ -1,56 +1,45 @@
 var Request = require('request');
-var readline = require('readline')
+var readline = require('readline');
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log('Welcome to our news application');
-console.log('1.ABC News \n 2.BBC Sports News \n 3.BBC News \n 4.CBC News ');
-console.log('Please choose preffered News source');
+function getTopheadlines(preferredNewsSource) {
+    const apiKey = `e1f6aaadd26d4ced846348e1222b23d7`;
+    const url = `https://newsapi.org/v2/top-headlines?sources=${preferredNewsSource}&apiKey=${apiKey}`;
+    Request.get(url, (error, response, body) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(JSON.parse(body));
+    });
+}
+const welcomeMessage = `Welcome to our news application
+1. ABC News
+2. Bloomberg
+3. BBC News
+4. CBC News
+Please choose preffered News source`;
+console.log(welcomeMessage)
 rl.question('Type option: ', function (option) {
     if (option === '1') {
         console.log('ABC News');
-        Request.get("https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=e1f6aaadd26d4ced846348e1222b23d7", (error, response, body) => {
-
-            if (error) {
-                return console.log(error);
-            }
-            console.log(JSON.parse(body));
-        });
+        getTopheadlines('abc-news');
     }
     else if (option === '2') {
-        console.log('BBC Sports News');
-        Request.get("https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=e1f6aaadd26d4ced846348e1222b23d7", (error, response, body) => {
-
-            if (error) {
-                return console.log(error);
-            }
-            console.log(JSON.parse(body));
-        });
+        console.log('Bloomberg');
+        getTopheadlines('bloomberg');
     }
     else if (option === '3') {
         console.log('BBC News');
-        Request.get("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=e1f6aaadd26d4ced846348e1222b23d7", (error, response, body) => {
-
-            if (error) {
-                return console.log(error);
-            }
-            console.log(JSON.parse(body));
-        });
+        getTopheadlines('bbc-news');
     }
     else if (option === '4') {
         console.log('CBC News');
-        Request.get("https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=e1f6aaadd26d4ced846348e1222b23d7", (error, response, body) => {
-
-            if (error) {
-                return console.log(error);
-            }
-            console.log(JSON.parse(body));
-        });
+        getTopheadlines('cbc-news');
     }
     else {
-        console.log('Invalid input')
-        rl.close();
+        throw new Error('Invalid input');
     }
     rl.close();
 });
